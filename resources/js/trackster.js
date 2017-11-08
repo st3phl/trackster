12901e2e65fa4595ba7abaf88ce99d1724944d0b
Stephanie Lun <stephanielun@Stephanies-MacBook-Pro.local>
2017-11-09 00:05:44 +0200

    Query the Last.fm API.

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,10 +1,11 @@
+var Trackster = {};
+const API_KEY = 'f0ad4bb16d502a38cc5c852ad6740ef5';
+
 $(document).ready(function() {
   $('#search-button').click(function() {
     Trackster.searchTracksByTitle($('#search-input').val());
   });
 });
-
-var Trackster = {};
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
@@ -19,5 +20,11 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
-
+  $.ajax({
+    url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
+    datatype: 'jsonp',
+    success: function(response) {
+      console.log('Success!');
+    }
+  });
 };
